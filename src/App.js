188fba3c0181ad76b67dea00711032fe01e919d1
Mayank Jha <mayank@arishi.agency>
2022-09-22 13:54:15 +0530

    Added lowercase function in TextForm

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Routes
-} from "react-router-dom";
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React from 'react';
 function App() {
   return (
     <>
       <Navbar title="TextUtils" about="About TextUtils" />
       <div className="container">
-             <TextForm heading="Enter the text"  title= "Text Area"/>
+        <TextForm heading="Enter the text" title="Text Area" />
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,15 +4,22 @@ import PropTypes from 'prop-types'
 export default function TextForm(props) {
 
     const handleUpClick = () => {
-
         let upText = text.toUpperCase();
         setText(upText);
+    }
+
+    const handleLowClick = () => {
+        let lowText = text.toLowerCase();
+        setText(lowText);
     }
 
     const handleOnChange = (event) => {
         setText(event.target.value)
     }
+
+
     const [text, setText] = useState("Enter the Text");
+
     return (
         <>
             <div className="container">
@@ -20,13 +27,19 @@ export default function TextForm(props) {
                 <h4>{props.title}</h4>
                 <div className="my-3">
                     <textarea className="form-control" id="mybox" rows="3" value={text} onChange={handleOnChange}></textarea>
-                    <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
+                    <button className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to uppercase</button>
+                    <button className="btn btn-primary mx-2 my-2" onClick={handleLowClick}>Convert to lowercase</button>
                 </div>
             </div>
             <div className="container">
                 <div className="my-3">
                     <h3>Text Summary</h3>
                     <p>Word Count is {(text.trim()).split(" ").length} and No. of characters is {text.length}</p>
+                    <p>Minutes to read is {0.008 * (text.trim()).split(" ").length}</p>
+                    <div className="my-3">
+                        <h4>Preview</h4>
+                        <p>{text}</p>
+                    </div>
                 </div>
             </div>
         </>
